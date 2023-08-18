53a6360c4825a63c70e50a5d5e6e1d5f004a2d17
Ameen2000 <Ameen2000@users.noreply.github.com>
2023-08-18 19:10:37 +0100

    Bug fixed in the Lexer.init function

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,6 @@ open Monkey
 open Core
 
 let () = 
-  let input = "();,{}" in
+  let input = "+=12let();,{}fn" in
   let tokens = Lexer.collect_tokens input |> List.map ~f:(Token.show) in
   List.iter ~f:print_endline tokens
